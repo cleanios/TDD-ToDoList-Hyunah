3052f8793f949e0d5f482421124d7a36f7ea9aec
hyunahprincess <aare1213@gmail.com>
2019-04-21 18:20:18 +0900

    init

---
+++ ToDoListWithTDD/ToDoListWithTDD/Sources/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ToDoListWithTDD
+//
+//  Created by Hyunah on 21/04/2019.
+//  Copyright © 2019 hyunable. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ ToDoListWithTDD/ToDoListWithTDDTests/ToDoListWithTDDTests.swift
@@ -0,0 +1,32 @@
+//
+//  ToDoListWithTDDTests.swift
+//  ToDoListWithTDDTests
+//
+//  Created by Hyunah on 21/04/2019.
+//  Copyright © 2019 hyunable. All rights reserved.
+//
+
+import XCTest
+@testable import ToDoListWithTDD
+
+class ToDoListWithTDDTests: XCTestCase {
+
+    override func setUp() {
+    }
+
+    override func tearDown() {
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
